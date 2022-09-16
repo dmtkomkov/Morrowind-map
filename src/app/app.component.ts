e61365c5df583883817d805dbf36165ee6655ae2
Dmitry Komkov <dmtkomkov@gmail.com>
2022-09-16 04:40:18 +0200

    Added two more layers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 enum ELayerSizes {
+  LAYER_SIZE_2 = 2,
   LAYER_SIZE_4 = 4,
   LAYER_SIZE_8 = 8,
+  LAYER_SIZE_16 = 16
 }
 
 type ILayers = {
   [key in ELayerSizes]: HTMLImageElement[][];
 }
 
-const MAX_ZOOM_LEVEL = 5;
-const MIN_ZOOM_LEVEL = -5;
+const MAX_ZOOM_LEVEL = 6;
+const MIN_ZOOM_LEVEL = -3;
 const ZOOM_FACTOR = 1.5;
 
 @Component({
@@ -18,22 +20,24 @@ const ZOOM_FACTOR = 1.5;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   @ViewChild('canvas', { static: true }) canvasRef: ElementRef<HTMLCanvasElement>;
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
-  cameraOffset: { x: number, y: number } = { x: 0, y: 0 };
-  zoomLevel: number = -5;
+  cameraOffset: { x: number, y: number } = { x: window.innerWidth - 1800, y: 0 };
+  zoomLevel: number = -3;
   cameraZoom: number = Math.pow(ZOOM_FACTOR, this.zoomLevel);
   isDragging: boolean = false;
   dragStart: { x: number, y: number } = { x: 0, y: 0 };
   initialPinchDistance: number | null = null
   update: boolean = true;
   layers: ILayers = {
+    [ELayerSizes.LAYER_SIZE_2]: [],
     [ELayerSizes.LAYER_SIZE_8]: [],
     [ELayerSizes.LAYER_SIZE_4]: [],
+    [ELayerSizes.LAYER_SIZE_16]: [],
   };
-  layerSize: ELayerSizes = ELayerSizes.LAYER_SIZE_4;
+  layerSize: ELayerSizes = ELayerSizes.LAYER_SIZE_2;
 
 
   ngOnInit(): void {
@@ -41,27 +45,22 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.canvas = this.canvasRef.nativeElement;
     this.ctx = this.canvas.getContext('2d')!;
 
-    for (let i = 0; i < ELayerSizes.LAYER_SIZE_8; i++) {
-      this.layers[ELayerSizes.LAYER_SIZE_8].push([]);
-      for (let j = 0; j < ELayerSizes.LAYER_SIZE_8; j++) {
-        const image: HTMLImageElement = new Image();
-        this.layers[ELayerSizes.LAYER_SIZE_8][i].push(image);
-      }
-    }
-
-    for (let i = 0; i < ELayerSizes.LAYER_SIZE_4; i++) {
-      this.layers[ELayerSizes.LAYER_SIZE_4].push([]);
-      for (let j = 0; j < ELayerSizes.LAYER_SIZE_4; j++) {
-        const image: HTMLImageElement = new Image();
-        this.layers[ELayerSizes.LAYER_SIZE_4][i].push(image);
-      }
-    }
+    this.pushLayers(ELayerSizes.LAYER_SIZE_2);
+    this.pushLayers(ELayerSizes.LAYER_SIZE_4);
+    this.pushLayers(ELayerSizes.LAYER_SIZE_8);
+    this.pushLayers(ELayerSizes.LAYER_SIZE_16);
 
     this.draw();
   }
 
-  ngAfterViewInit(): void {
-
+  private pushLayers(size: ELayerSizes) {
+    for (let i = 0; i < size; i++) {
+      this.layers[size].push([]);
+      for (let j = 0; j < size; j++) {
+        const image: HTMLImageElement = new Image();
+        this.layers[size][i].push(image);
+      }
+    }
   }
 
   draw() {
@@ -137,11 +136,17 @@ export class AppComponent implements OnInit, AfterViewInit {
         return;
       }
 
-      if (this.zoomLevel > 1) {
+      if (this.zoomLevel > 4) {
+        this.layerSize = ELayerSizes.LAYER_SIZE_16;
+        this.cameraZoom = Math.pow(ZOOM_FACTOR, this.zoomLevel)/8
+      } else if (this.zoomLevel > 2) {
         this.layerSize = ELayerSizes.LAYER_SIZE_8;
-        this.cameraZoom = Math.pow(ZOOM_FACTOR, this.zoomLevel)/2;
-      } else if (this.zoomLevel <= 1) {
+        this.cameraZoom = Math.pow(ZOOM_FACTOR, this.zoomLevel)/4;
+      } else if (this.zoomLevel > -1) {
         this.layerSize = ELayerSizes.LAYER_SIZE_4;
+        this.cameraZoom = Math.pow(ZOOM_FACTOR, this.zoomLevel)/2;
+      } else {
+        this.layerSize = ELayerSizes.LAYER_SIZE_2;
         this.cameraZoom = Math.pow(ZOOM_FACTOR, this.zoomLevel);
       }
 
