daba1ce0953b83c28992740acafeb0ce1027a500
Dmitry Komkov <dmtkomkov@gmail.com>
2022-09-18 17:43:48 +0200

    Improve download script

--- download.js
+++ download.js
@@ -1,13 +1,17 @@
 let fs = require('fs'), https = require('https'), Stream = require('stream').Transform;
 
-let url_base = 'https://d38oy7iu7t2hpf.cloudfront.net/32/';
+const layers = [
+  { dir: 'src/assets/2/', url_base: 'https://d38oy7iu7t2hpf.cloudfront.net/64/', tiles: 2 },
+  { dir: 'src/assets/4/', url_base: 'https://d38oy7iu7t2hpf.cloudfront.net/32/', tiles: 4 },
+  { dir: 'src/assets/8/', url_base: 'https://d38oy7iu7t2hpf.cloudfront.net/16/', tiles: 8 },
+  { dir: 'src/assets/16/', url_base: 'https://d38oy7iu7t2hpf.cloudfront.net/8/', tiles: 16 }
+]
 
-console.log('start');
 
-function getImage(i,j) {
+function getImage(url_base, dir, i, j) {
     console.log(i,j)
-    let url = url_base + `${i}-${j}.webp`
-    https.request(url, function(response) {
+    let src = url_base + `${i}-${j}.webp`
+    https.request(src, function(response) {
         let data = new Stream();
 
         response.on('data', function(chunk) {
@@ -15,16 +19,23 @@ function getImage(i,j) {
         });
 
         response.on('end', function() {
-            let name = 'src/assets/4/image-' + `${i}-${j}.webp`;
-            console.log('download', name, 'from', url);
-            fs.writeFileSync(name, data.read());
+            let dst = dir + 'image-' + `${i}-${j}.webp`;
+            console.log('download', dst, 'from', src);
+            fs.writeFileSync(dst, data.read());
         });
     }).end();
 }
 
+layers.forEach(layer => {
+  if (!fs.existsSync(layer.dir)){
+    fs.mkdirSync(layer.dir);
+  }
 
-for (let i=0; i < 4; i++) {
-    for (let j=0; j < 4; j++) {
-        getImage(i,j);
+  for (let i=0; i < layer.tiles; i++) {
+    for (let j=0; j < layer.tiles; j++) {
+      getImage(layer.url_base, layer.dir, i, j);
     }
-}
+  }
+})
+
+
