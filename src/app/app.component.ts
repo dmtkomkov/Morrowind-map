a5d2e364d6bcf5f420a53cdd9183dea6551fbeee
Dmitry Komkov <dmtkomkov@gmail.com>
2022-09-24 05:41:09 +0200

    Optimized images and location rendering;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 enum ELayerSize {
   LAYER_SIZE_2 = 2,
@@ -7,11 +8,17 @@ enum ELayerSize {
   LAYER_SIZE_16 = 16
 }
 
+enum EImageStatus {
+  INIT,
+  LOADING,
+  LOADED,
+}
+
 const ORIGINAL_TILE_SIZE = 2048;
 const ANIMATION_TIME = 200;
 
 type ILayers = {
-  [key in ELayerSize]: HTMLImageElement[][];
+  [key in ELayerSize]: IImageLoader[][];
 }
 
 const MAX_ZOOM_LEVEL = 6;
@@ -23,6 +30,11 @@ const LOCATIONS = [
   { x: 1110, y: 1050, name: 'Some Town 2'},
   { x: 1055.5, y: 968.6, name: 'The Temple'}
 ]
+
+interface IImageLoader {
+  image: HTMLImageElement;
+  status: EImageStatus;
+}
 
 @Component({
   selector: 'app-root',
@@ -49,9 +61,12 @@ export class AppComponent implements OnInit {
   cameraOffsetDeltaX: number = 0;
   cameraOffsetDeltaY: number = 0;
   cameraZoom: number = 0;
+  cameraOffsetX: number = 0;
+  cameraOffsetY: number = 0;
   layerSize: ELayerSize = ELayerSize.LAYER_SIZE_2;
   mapX: string = 'unknown';
   mapY: string = 'unknown';
+  imgLoadingCount: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   ngOnInit(): void {
     this.canvas = this.canvasRef.nativeElement;
@@ -60,6 +75,13 @@ export class AppComponent implements OnInit {
       this.pushImages(layerSize as number);
     })
     this.draw();
+
+    // Draw location on top of visible tiles only
+    this.imgLoadingCount.subscribe( {
+      next: (count: number) => {
+        if (count === 0) this.drawLocations();
+      },
+    });
   }
 
   private pushImages(size: ELayerSize) {
@@ -67,7 +89,7 @@ export class AppComponent implements OnInit {
       this.layers[size].push([]);
       for (let j = 0; j < size; j++) {
         const image: HTMLImageElement = new Image();
-        this.layers[size][i].push(image);
+        this.layers[size][i].push({ image, status: EImageStatus.INIT });
       }
     }
   }
@@ -79,10 +101,10 @@ export class AppComponent implements OnInit {
       this.canvas.width = window.innerWidth;
       this.canvas.height = window.innerHeight;
 
-      const cameraOffsetX = Math.floor(this.cameraOffset.x - this.cameraOffsetDeltaX * (updateTime/ANIMATION_TIME));
-      const cameraOffsetY = Math.floor(this.cameraOffset.y - this.cameraOffsetDeltaY * (updateTime/ANIMATION_TIME));
+      this.cameraOffsetX = Math.floor(this.cameraOffset.x - this.cameraOffsetDeltaX * (updateTime/ANIMATION_TIME));
+      this.cameraOffsetY = Math.floor(this.cameraOffset.y - this.cameraOffsetDeltaY * (updateTime/ANIMATION_TIME));
 
-      this.ctx.translate(cameraOffsetX, cameraOffsetY);
+      this.ctx.translate(this.cameraOffsetX, this.cameraOffsetY);
 
       if (this.newZoomLevel > 4) this.layerSize = ELayerSize.LAYER_SIZE_16;
       else if (this.newZoomLevel > 2) this.layerSize = ELayerSize.LAYER_SIZE_8;
@@ -94,57 +116,74 @@ export class AppComponent implements OnInit {
       this.cameraZoom = oldCameraZoom + (newCameraZoom - oldCameraZoom) * (updateTime/ANIMATION_TIME);
       const tileSize = ORIGINAL_TILE_SIZE * this.cameraZoom;
 
-      const minVisibleTileX = Math.max(Math.floor((0 - cameraOffsetX)/tileSize), 0);
-      const minVisibleTileY = Math.max(Math.floor((0 - cameraOffsetY)/tileSize), 0);
-      const maxVisibleTileX = Math.min(Math.floor((window.innerWidth - cameraOffsetX)/tileSize), this.layerSize - 1);
-      const maxVisibleTileY = Math.min(Math.floor((window.innerHeight - cameraOffsetY)/tileSize), this.layerSize - 1);
+      const minVisibleTileX = Math.max(Math.floor((0 - this.cameraOffsetX)/tileSize), 0);
+      const minVisibleTileY = Math.max(Math.floor((0 - this.cameraOffsetY)/tileSize), 0);
+      const maxVisibleTileX = Math.min(Math.floor((window.innerWidth - this.cameraOffsetX)/tileSize), this.layerSize - 1);
+      const maxVisibleTileY = Math.min(Math.floor((window.innerHeight - this.cameraOffsetY)/tileSize), this.layerSize - 1);
 
       for (let tileX = minVisibleTileX; tileX <= maxVisibleTileX; tileX++) {
         for (let tileY = minVisibleTileY; tileY <= maxVisibleTileY; tileY++) {
-          const image = this.layers[this.layerSize][tileX][tileY];
-          if (!image.src) image.src = `assets/${this.layerSize}/image-${tileX}-${tileY}.webp`;
-          this.drawImageTile(image, tileX, tileY, tileSize);
+          const imageLoader: IImageLoader = this.layers[this.layerSize][tileX][tileY];
+          if (!imageLoader.image.src) imageLoader.image.src = `assets/${this.layerSize}/image-${tileX}-${tileY}.webp`;
+          this.drawImageTile(imageLoader, tileX, tileY, tileSize);
         }
       }
-
-      this.drawLocations();
 
       if (ANIMATION_TIME - updateTime <= 0) {
         this.oldZoomLevel = this.newZoomLevel;
         this.startZoomTime = 0;
-        this.cameraOffset.x = cameraOffsetX;
-        this.cameraOffset.y = cameraOffsetY;
+        this.cameraOffset.x = this.cameraOffsetX;
+        this.cameraOffset.y = this.cameraOffsetY;
         this.cameraOffsetDeltaX = 0;
         this.cameraOffsetDeltaY = 0;
         this.update = false;
       }
+
+      this.drawLocations();
     }
 
     requestAnimationFrame( () => this.draw() );
   }
 
-  drawImageTile(image: HTMLImageElement, tileX: number, tileY: number, tileSize: number) {
-    if (image.complete) {
-      this.ctx.drawImage(image, tileX * tileSize, tileY * tileSize, tileSize, tileSize);
-    } else {
-      image.onload = () => {
-        this.ctx.drawImage(image, tileX * tileSize, tileY * tileSize, tileSize, tileSize);
+  drawImageTile(imageLoader: IImageLoader, tileX: number, tileY: number, tileSize: number) {
+    if (imageLoader.status === EImageStatus.LOADED) {
+      this.ctx.drawImage(imageLoader.image, tileX * tileSize, tileY * tileSize, tileSize, tileSize);
+    } else if (imageLoader.status === EImageStatus.INIT) {
+      imageLoader.status = EImageStatus.LOADING;
+      this.imgLoadingCount.next(this.imgLoadingCount.value + 1);
+      imageLoader.image.onload = () => {
+        imageLoader.status = EImageStatus.LOADED;
+        this.ctx.drawImage(imageLoader.image, tileX * tileSize, tileY * tileSize, tileSize, tileSize);
+        this.imgLoadingCount.next(this.imgLoadingCount.value - 1);
       }
     }
   }
 
   drawLocations() {
     const zoom: number = this.cameraZoom * this.layerSize;
+    const startX: number = (0 - this.cameraOffsetX) / zoom;
+    const startY: number = (0 - this.cameraOffsetY) / zoom;
+    const endX: number = (window.innerWidth - this.cameraOffsetX) / zoom;
+    const endY: number = (window.innerHeight - this.cameraOffsetY) / zoom;
     LOCATIONS.forEach((loc) => {
       const { x, y, name } = loc;
-      this.ctx.beginPath();
-      this.ctx.strokeStyle = '#ffcf12';
-      this.ctx.lineWidth = 2;
-      this.ctx.rect(x * zoom - 4, y * zoom - 4, 7, 7);
-      this.ctx.stroke();
-      this.ctx.font = '16px MagicCards';
-      this.ctx.fillStyle = '#ffcf12';
-      this.ctx.fillText(name, x * zoom + 10, y * zoom + 4);
+      if (x > startX && x < endX && y > startY && y < endY) {
+        this.ctx.fillStyle = '#ffcf12';
+        this.ctx.strokeStyle = '#ffcf12';
+        this.ctx.font = '16px MagicCards';
+
+        this.ctx.beginPath();
+        this.ctx.lineWidth = 2;
+        this.ctx.rect(x * zoom - 4, y * zoom - 4, 7, 7);
+        this.ctx.stroke();
+
+        const textWidth = this.ctx.measureText(name).width + 10 ;
+        if (textWidth / zoom > endX - x) {
+          this.ctx.fillText(name, x * zoom - textWidth, y * zoom + 4);
+        } else {
+          this.ctx.fillText(name, x * zoom + 10, y * zoom + 4);
+        }
+      }
     });
   }
 
